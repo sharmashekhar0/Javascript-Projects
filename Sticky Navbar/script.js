d4f5915da40ba1cc7f83e4cc5fe238765d41a7f7
sharmashekhar0 <beingperfect76@gmail.com>
2023-09-25 17:39:51 +0530

    Sticky Navbar Project Added

---
+++ Sticky Navbar/script.js
@@ -0,0 +1,15 @@
+
+const navbar = document.querySelector(".navbar");
+const bottomContainer = document.querySelector(".bottom-container");
+console.log(navbar.offsetHeight);
+console.log(bottomContainer.offsetTop);
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > (bottomContainer.offsetTop - navbar.offsetHeight)) {
+        navbar.classList.add("active");
+    } else {
+        navbar.classList.remove("active");
+    }
+   
+});

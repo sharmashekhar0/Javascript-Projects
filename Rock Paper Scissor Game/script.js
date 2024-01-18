881886bd318d18d7bfec6604bdff46189b598617
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-18 22:37:30 +0530

    add UI changes

--- Rock Paper Scissor Game/script.js
+++ Rock Paper Scissor Game/script.js
@@ -10,64 +10,54 @@ let yourScoreValue = 0;
 let computerScoreValue = 0;
 
 rock.addEventListener("click", () => {
+	const compChoice = Math.ceil(Math.random() * 3);
+	console.log("Number = ", compChoice);
 
-    const compChoice = Math.ceil(Math.random() * 3);
-    console.log("Number = ", compChoice);
-
-    if(compChoice === 1) {
-        message.textContent = "It's a Tie"
-    }
-    else if(compChoice === 2) {
-        message.textContent = "You Lose! Paper beats Rock"
-        computerScoreValue++;
-        computerScore.textContent = computerScoreValue;
-    }
-    else {
-        message.textContent = "You Win! Rock beats Scissor"
-        yourScoreValue++;
-        yourScore.textContent = yourScoreValue;
-    }
-    message.classList.remove("hide");
+	if (compChoice === 1) {
+		message.textContent = "It's a Tie";
+	} else if (compChoice === 2) {
+		message.textContent = "You Lose! Paper beats Rock";
+		computerScoreValue++;
+		computerScore.textContent = computerScoreValue;
+	} else {
+		message.textContent = "You Win! Rock beats Scissor";
+		yourScoreValue++;
+		yourScore.textContent = yourScoreValue;
+	}
+	message.classList.remove("hide");
 });
 
 paper.addEventListener("click", () => {
+	const compChoice = Math.ceil(Math.random() * 3);
+	console.log("Number = ", compChoice);
 
-    const compChoice = Math.ceil(Math.random() * 3);
-    console.log("Number = ", compChoice);
-
-    if(compChoice === 2) {
-        message.textContent = "It's a Tie"
-    }
-    else if(compChoice === 1) {
-        message.textContent = "You Win! Paper beats Rock"
-        yourScoreValue++;
-        yourScore.textContent = yourScoreValue;
-    }
-    else {
-        message.textContent = "You Lose! Scissor beats Paper"
-        computerScoreValue++;
-        computerScore.textContent = computerScoreValue;
-    }
-    message.classList.remove("hide");
+	if (compChoice === 2) {
+		message.textContent = "It's a Tie";
+	} else if (compChoice === 1) {
+		message.textContent = "You Win! Paper beats Rock";
+		yourScoreValue++;
+		yourScore.textContent = yourScoreValue;
+	} else {
+		message.textContent = "You Lose! Scissor beats Paper";
+		computerScoreValue++;
+		computerScore.textContent = computerScoreValue;
+	}
+	message.classList.remove("hide");
 });
 
 scissor.addEventListener("click", () => {
+	const compChoice = Math.ceil(Math.random() * 3);
 
-    const compChoice = Math.ceil(Math.random() * 3);
-
-    if(compChoice === 3) {
-        message.textContent = "It's a Tie"
-    }
-    else if(compChoice === 1) {
-        message.textContent = "You Lose! Rock beats Scissor"
-        computerScoreValue++;
-        computerScore.textContent = computerScoreValue;
-    }
-    else {
-        message.textContent = "You Win! Scissor beats Paper"
-        yourScoreValue++;
-        yourScore.textContent = yourScoreValue;
-    }
-    message.classList.remove("hide");
-    
+	if (compChoice === 3) {
+		message.textContent = "It's a Tie";
+	} else if (compChoice === 1) {
+		message.textContent = "You Lose! Rock beats Scissor";
+		computerScoreValue++;
+		computerScore.textContent = computerScoreValue;
+	} else {
+		message.textContent = "You Win! Scissor beats Paper";
+		yourScoreValue++;
+		yourScore.textContent = yourScoreValue;
+	}
+	message.classList.remove("hide");
 });
